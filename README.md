255e6bb432cb0ddb2bc1f6352e9802896ed208f1
Deepika Thudumu <deepikathudumu24@gmail.com>
2025-01-24 01:44:20 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,35 @@
+# Movie Search App
+
+A simple web application built with React and Vite that allows users to search for movies using a search bar. The app uses a movie database API to retrieve movie data based on user queries and displays relevant movie details.
+
+## Features
+
+- Search for movies by title using a search bar.
+- Display movie posters, titles, and release years.
+- Responsive design for various screen sizes.
+- Use of a movie database API to fetch movie data.
+
+## Technologies Used
+
+- **React**: A JavaScript library for building user interfaces.
+- **Vite**: A next-generation, fast, and lean build tool.
+- **CSS**: For styling the application.
+
+## Getting Started
+
+### Prerequisites
+
+Ensure that you have [Node.js](https://nodejs.org/) installed on your system.
+
+## Usage
+
+1. Type the name of a movie into the search bar.
+2. The app will display a list of movies matching your search query.
+3. Hover on a movie to view more details.
+
+## Project Structure
+
+- `src/`: Contains the source code of the project.
+  - `assets/`: React assets such as `MovieCard`.
+  - `App.jsx`: Main entry point of the application.
+  - `api.js`: Handles API requests to the movie database.
---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import './App.css'
+import SearchIcon from "./search.svg"
+import MovieCard from "./MovieCard";
+
+const API_URL = "http://www.omdbapi.com?apikey=b169cf0";
+
+function App() {
+  const [movies, setMovies] = useState([])
+  const [serachTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Baahubali");
+  }, []);
+
+
+  return (
+    <>
+      <div className="app">
+        <h1>Movies</h1>
+
+        <div className="search">
+          <input placeholder="Search for movies"
+            value={serachTerm}
+            onChange={(e) => setSearchTerm(e.target.value)} />
+
+          <img src={SearchIcon} alt="search" onClick={() => searchMovies(serachTerm)} />
+        </div>
+
+        {movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No Movies Found</h2>
+          </div>
+        )}
+        <h2>A react project by deepika thudumu</h2>
+
+      </div>
+    </>
+  )
+}
+
+export default App
